33aa3fd9a23639a8de6b42990b5e910a503fde24
Giftea <eleojogiftuhiene@gmail.com>
2022-08-14 00:56:02 +0100

    create event

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contractAddress = "0x6D34Fef5D90EeF8383432eB124C98eDcb832C66a";
+  const contractABI = abiJSON.abi;
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
